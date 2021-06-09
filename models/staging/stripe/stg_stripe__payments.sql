b158dd6c2594482c137230e288646e9daf63d9c8
Sivakkamy Mathu <sivakkamy_mathu@condenast.com>
2021-06-09 12:16:08 +0000

    Day 1 Activities for Customer Life Time Values

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,20 @@
+with
+source_payment as 
+(
+    select * from raw.stripe.payment
+
+),
+
+transformed as 
+(
+    select 
+        id as payment_id, 
+        orderid as order_id,
+        paymentmethod as payment_method, 
+        status as payment_status,
+        amount / 100.0 as amount,
+        created as created_date
+    from source_payment
+)
+
+select * from transformed
